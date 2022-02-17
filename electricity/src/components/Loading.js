49e2dea4ca0b15e5f6283e15204e325b522198b8
dianKoliva <dianpretty01@gmail.com>
2022-02-17 16:27:03 +0200

    new changes

--- electricity/src/components/Loading.js
+++ electricity/src/components/Loading.js
@@ -3,12 +3,20 @@ import React, { useState } from 'react'
 const Loading = () => {
     const [token,setToken]=useState(0)
   return (
-    <div className=''>Enter your token for electricity
+    <div className='cnt bg-white w-96 mt-20'>
+      
+      <p className='text-center pt-4 pb-4'>Add electricity </p>
+
+      <div className='ml-20'>
+
+      Enter your token for electricity
 
 <div className='block'>
-<input type="text" ></input>
+<input className='input px-2 py-1 w-56 mt-1' type="text" ></input>
 <br></br>
-     <button className='bg-red-500'>Empower</button>
+     <button className=' mt-4 mb-6  get w-56 py-1 bg-red-500'>Save</button>
+      </div>
+
 </div>
 
 
