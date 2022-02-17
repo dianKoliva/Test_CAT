2d4015949a9021b4610a8b3e9e164a9f5a9c5507
dianKoliva <dianpretty01@gmail.com>
2022-02-17 14:43:08 +0200

    new changes

---
+++ electricity/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+
+import Buying from "./components/Buying"
+
+function App() {
+  return (
+    <div className="App">
+   <Buying></Buying>
+   
+    </div>
+  );
+}
+
+export default App;
---
+++ electricity/src/components/Buying.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react'
+
+const Buying = () => {
+  const [nbr,setNbr]=useState(0);
+  const [money,setMoney]=useState(0)
+  const [invMeter,setInvMeter]=useState(false)
+  const [nothundred,setNothundred]=useState(false)
+  const [much,setMuch]=useState(false)
+  const [show,changeShow]=useState(false)
+  const [token,setToken]=useState(true)
+  const submit=()=>{
+    if(length(nbr)>6){
+     setInvMeter(true)
+    }
+    else if(money%100!=0){
+ setNothundred(true)
+ setInvMeter(false)
+    }
+    else if(money>182500){
+      setMuch(true)
+      setNothundred(false)
+      setInvMeter(false)
+    }
+  }
+  return (
+    <div> 
+    <div className="ml-96 pl-52 mt-5 ">Buying electricity Tokens
+
+<div className="block">
+  {invMeter?<p className='text-red'>invalid meter, only 6 digits accepted</p>:null}
+<label>
+    Meter number
+    </label>
+<input onChange={(e)=>{setNbr(e.target.value)}} value={nbr} type="text" />
+</div>
+    
+
+    <div className="block">
+<label>
+    Amount of Money
+    </label>
+<input onChange={(e)=>{setMoney(e.target.value)}} value={money} type="text" />
+</div>
+{show? <p>Token bought: {token}</p>:null}
+<button onClick={()=>submit()}>Get token</button>
+    </div>
+
+    </div>
+  )
+}
+
+export default Buying
