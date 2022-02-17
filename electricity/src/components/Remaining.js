072cf1745b614b4927ab576ee5d056a9c6ea514f
dianKoliva <dianpretty01@gmail.com>
2022-02-17 15:18:02 +0200

    new changes

--- electricity/src/components/Remaining.js
+++ electricity/src/components/Remaining.js
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 
 const Remaining = () => {
+    const [meter,setMeter]=useState(0);
+    const [invalid,setinvalid]=useState(false)
+
+    const submit=()=>{
+setinvalid(false)
+if(meter.length!=6||typeof meter!="string"){
+setinvalid(true);
+
+}
+    }
+
   return (
     <div>
-        <p>Checking your remaining days</p>
+        <p>Check Balance</p>
 
+ <div>
+     <p className="">Enter meter number</p>
+     <input value={meter} onChange={(e)=>setMeter(e.target.value)} className=' focus:outline-none '></input>
+ </div>
+
+ <button onClick={
+    ()=>submit() 
+ } className=' bg-red-500'>Check balance</button>
 
     </div>
   )
